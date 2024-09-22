75f3fb92d19526b5fbab4739a83adefd08caa039
yuherlim <110385648+yuherlim@users.noreply.github.com>
2024-09-22 20:53:55 +0800

    added get_payments, removed testing code

--- app.py
+++ app.py
@@ -6,33 +6,17 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+
 # Database connection function
 def get_db_connection():
     connection = mysql.connector.connect(
-        host="localhost",      # XAMPP MySQL host
-        user="root",           # Default MySQL user
-        password="",           # Default MySQL password (empty)
-        database="payment_web_service"    # Your database name
+        host="localhost",  # XAMPP MySQL host
+        user="root",  # Default MySQL user
+        password="",  # Default MySQL password (empty)
+        database="payment_web_service"  # Your database name
     )
     return connection
 
-# Testing
-class User:
-    def __init__(self, id, name, email):
-        self.id = id
-        self.name = name
-        self.email = email
-        self.test = {'test1','test2'}
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'email': self.email,
-            'tests': [test for test in self.test],
-            'class': {'attr1':'test','attr2':'test','attr3':'test',}
-
-        }
 
 class Payment:
     def __init__(self, paymentID, transactionID, paymentDateTime, paymentMethod, paymentAmount):
@@ -41,6 +25,15 @@ class Payment:
         self.paymentDateTime = paymentDateTime
         self.paymentMethod = paymentMethod
         self.paymentAmount = paymentAmount
+
+    def to_dict(self):
+        return {
+            'paymentID': self.paymentID,
+            'transactionID': self.transactionID,
+            'paymentDateTime': self.paymentDateTime,
+            'paymentMethod': self.paymentMethod,
+            'paymentAmount': self.paymentAmount,
+        }
 
 
 @app.route('/api/validate-payment', methods=['POST'])
@@ -156,45 +149,29 @@ def validate_payment():
         return jsonify([{"error": "Internal server error"}]), 500
 
 
-
-
-@app.route('/api/users', methods=['GET'])
-def get_users():
+@app.route('/api/get-payments', methods=['GET'])
+def get_payments():
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM tests")
-    users_data = cursor.fetchall()
+    cursor.execute("SELECT * FROM payment")
+    payments_data = cursor.fetchall()
     cursor.close()
     conn.close()
-    print(users_data)
-    # Create an array of User objects
-    users = []
-    for user in users_data:
-        # Instantiate a User object with data from the database
-        user_obj = User(
-            id=user['id'],
-            name=user['name'],
-            email=user['email']
+    print(payments_data)
+    # Create an array of Payment objects
+    payments = []
+    for payment in payments_data:
+        # Instantiate a Payment object with data from the database
+        payment_obj = Payment(
+            paymentID=payment['paymentID'],
+            transactionID=payment['transactionID'],
+            paymentDateTime=payment['paymentDateTime'],
+            paymentMethod=payment['paymentMethod'],
+            paymentAmount=payment['paymentAmount'],
         )
-        users.append(user_obj.to_dict())
+        payments.append(payment_obj.to_dict())
 
-    return jsonify(users)
-
-@app.route('/api/user', methods=['POST'])
-def add_user():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    new_user = request.json
-
-    name = new_user['name']
-    email = new_user['email']
-
-    cursor.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (name, email))
-    conn.commit()
-
-    cursor.close()
-    conn.close()
-    return jsonify({"message": "User added successfully!"}), 201
+    return jsonify(payments)
 
 
 # Define a route for the service
@@ -207,10 +184,12 @@ def api():
     }
     return jsonify(data)
 
+
 # Define another route with parameters
 @app.route('/api/greet/<name>', methods=['GET'])
 def greet(name):
     return jsonify({"message": f"Hello, {name}!"})
+
 
 # Run the service
 if __name__ == '__main__':
